df418f41f5dc9b3e20a77455331bda281ee6dd04
agatha-chituwa <agathachituwa2@gmail.com>
2025-04-25 10:43:44 +0200

    predictor and get character exercise

--- src/SalaryPredictor.java
+++ src/SalaryPredictor.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
-
 public class SalaryPredictor {
+    public static void main(String [] args){
+        int tuition = 10000;
+
+        //tracking given fees we want to know in 10 years how will the fees in increase if increase by 7%
+        //in 10 years how much will the fees be lets start for a year
+
+
+        // in 10 years
+        int n = 0;
+        while ( tuition < 20000){
+            int increase = (int) ((7.0/100) * tuition);
+            tuition = (tuition + increase);
+            System.out.println("Tuition will be" + tuition );
+            n++;
+        }
+        System.out.println("it will take " + n + " years");
+
+
+    }
 }
--- src/getCharacter.java
+++ src/getCharacter.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
-
 public class getCharacter {
+    public static  void main(String [] args){
+        String s = "moon";
+        char firstCharacter = s.charAt(0);
+        String name = "";
+        for(int i = 0; i< s.length(); i++){
+            int l = s.length() - i;
+
+            if(s.charAt(i)  == s.charAt(l-1)){
+                name = "palindrome";
+            }else {
+                name = "not palindrome";
+            }
+        }
+        System.out.println(s + " is " + name);
+    }
 }
